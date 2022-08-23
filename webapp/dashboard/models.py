fdf34ebbb079c0eea9425095efb0dd3a82fa50c9
Nikola Knežević <48497634+knez@users.noreply.github.com>
2022-08-23 22:07:16 +0200

    Basic skeleton for dashboard

--- webapp/dashboard/models.py
+++ webapp/dashboard/models.py
@@ -2,7 +2,8 @@ from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(100), unique=True)
+    notify = db.Column(db.Boolean)
