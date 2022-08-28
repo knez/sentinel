31072ec9a402dc3abe3d8fc3192370048558682d
Nikola Knežević <48497634+knez@users.noreply.github.com>
2022-08-29 00:30:01 +0200

    Simplify auth logic

--- dashboard/auth.py
+++ dashboard/auth.py
@@ -10,6 +10,7 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+
 @auth.route('/login', methods=['POST'])
 def login_post():
     # login code goes here
@@ -25,31 +26,6 @@ def login_post():
     login_user(user)
     return redirect(url_for('main.dashboard'))
 
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    # code to validate and add user to database goes here
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 @login_required
