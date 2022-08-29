decb97cf058b33e60fc7b6dd814f9ddfd1fc8d08
Nikola Knežević <48497634+knez@users.noreply.github.com>
2022-08-29 18:00:00 +0200

    Fix modal windows

--- dashboard/static/modal.js
+++ dashboard/static/modal.js
@@ -1,33 +1,19 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal;
 
-// Get the button that opens the modal
-var btn1 = document.getElementById("video-1");
-var btn2 = document.getElementById("video-2");
-var btn3 = document.getElementById("video-3");
-var btn4 = document.getElementById("video-4");
+function activateModal(id) {
+  modal = document.getElementById(id);
+  modal.style.display = "block";
+}
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn1.onclick = function () {
-  modal.style.display = "block";
-};
-btn2.onclick = function () {
-  modal.style.display = "block";
-};
-btn3.onclick = function () {
-  modal.style.display = "block";
-};
-btn4.onclick = function () {
-  modal.style.display = "block";
-};
-
+var spanList = document.getElementsByClassName("close");
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+for (var i = 0; i < spanList.length; i++ ) {
+  spanList[i].onclick = function () {
+    modal.style.display = "none";
+  };
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
