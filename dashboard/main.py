9bcf32305bf50f640f7dcb6b33511e5eca27da5a
Nikola Knežević <48497634+knez@users.noreply.github.com>
2022-08-29 15:31:34 +0200

    Implement backend for notification settings

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
+from .models import User
 from . import db
 
 main = Blueprint('main', __name__)
@@ -11,20 +12,30 @@ def index():
 @main.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.name)
+    return render_template('dashboard.html')
 
 @main.route('/dashboard/notifications')
 @login_required
 def notifications():
-    return render_template('notification.html', name=current_user.name)
+    user = User.query.filter_by(name=current_user.name).first()
+    return render_template('notification.html', email=user.email, notify=user.notify)
+
+@main.route('/dashboard/notifications', methods=['POST'])
+@login_required
+def notifications_update():
+    enabled = request.form.get('enabled')
+    email = request.form.get('email')
+    # update user preferences
+    user = User.query.filter_by(name=current_user.name).first()
+    user.email = email
+    user.notify = True if enabled else False
+    # save to database
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('main.notifications'))
 
 
 # Define API endpoints
-@main.route('/api/update', methods=['POST'])
-def update():
-    # TODO
-    return ''
-
 @main.route('/api/delete')
 def delete():
     # TODO
